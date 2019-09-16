e9693869f84600a0854f28edd40bb5d7b3c03efe
PaulReedSmith <schwarztig@gmail.com>
2019-09-17 00:59:29 +0300

    add comander.js config

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,10 @@ import { version } from '../../package.json';
 
 program
   .version(version)
-  .description('Calculates differences between two files');
-
-func();
+  .description('Calculates differences between two files')
+  .arguments('<<firstConfig> <secondConfig>')
+  .option('-V, --version        output the version number')
+  .option('-h, --help           output usage information')
+  .option('-f, --format [type]  Output format')
+  .action((firstConfig, secondConfig) => console.log(func(firstConfig, secondConfig)))
+  .parse(process.argv);
