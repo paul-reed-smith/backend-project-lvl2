6da248985b579949948f47619750d73c15045f1f
PaulReedSmith <schwarztig@gmail.com>
2019-09-17 00:45:02 +0300

    add comander options

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+import program from 'commander';
 import func from '../index';
+import { version } from '../../package.json';
+
+program
+  .version(version)
+  .description('Calculates differences between two files');
 
 func();
