96e185c3224b34b9b044f9b5567d16f501aa292d
PaulReedSmith <schwarztig@gmail.com>
2019-09-27 19:12:56 +0300

    del utils.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,10 +3,10 @@ import gendiff from '../src';
 
 
 test('gendiff', () => {
-  const before = fs.readFileSync(`${__dirname}/__fixtures__/before.json`);
-  const after = fs.readFileSync(`${__dirname}/__fixtures__/after.json`);
+  const before = `${__dirname}/__fixtures__/before.json`;
+  const after = `${__dirname}/__fixtures__/after.json`;
   const expected = fs.readFileSync(`${__dirname}/__fixtures__/expexted.txt`);
   const generated = gendiff(before, after);
 
-  expect(expected).not.toEqual(generated);
+  expect(expected).toEqual(generated);
 });
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-const isEmptyObject = (obj) => Object.keys(obj).length === 0;
-
-const readFile = (path) => fs.readFileSync(path); // realy need this?
-
-export { isEmptyObject, readFile };
