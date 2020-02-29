c16dbcbf28a9c7452ca7d35d6ffdb987da268ba6
Панин Дмитрий <dmitry@MacBook-Pro-Panin.local>
2020-02-29 18:27:11 +0300

    add pretty format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,12 @@
-// { type: 'added', key, value: data2[key] }
-// { type: 'deleted', key, value: data1[key] }
-// { type: 'changed', key, value1: data1[key], value2: data2[key]}
-// { type: 'unchanged', key, value2: data2[key] }
-// { type: 'nested', key, children: f(data1[key], data2[key]) }
+import prettyRender from './pretty';
 
-export default () => true;
+
+const getRender = (format) => {
+  const formatters = {
+    pretty: prettyRender,
+  };
+
+  return formatters[format];
+};
+
+export default getRender;
