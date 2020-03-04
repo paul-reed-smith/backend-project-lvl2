2fbf8da2218fb64f3c37e80c6b6c631edbc8a921
paul-reed-smith <forall@mm.st>
2020-03-04 22:08:06 +0300

    refact pretty formater

--- src/formatters/pretty.js
+++ src/formatters/pretty.js
@@ -1,24 +1,51 @@
-const douleIndent = '    ';
+import _ from 'lodash';
+
+const indentCalc = (nestingLevel) => {
+  const res = '  '.repeat(nestingLevel);
+  return res;
+};
+
+const valueChecker = (value, nestingLevel) => {
+  if (_.isObject(value)) {
+    const keys = Object.keys(value);
+    console.log(`value=${value}, nestingLevel=${nestingLevel}`);
+    const mapped = keys.map((el) => `${indentCalc(nestingLevel + 3)}${el}: ${value[el]}`);
+    const stringed = mapped.join('\n');
+
+    return `{ \n${stringed}\n${indentCalc(nestingLevel + 1)}}`;
+  }
+
+
+  return value;
+};
 
 const renderTypes = {
-  nested: ({ key, children }, indent, func) => `${indent}${key}: \n${func(children, indent)}\n`,
-  changed: ({ key, value1, value2 }, indent) => `${indent}+ ${key}: ${value2}\n${indent}- ${key}: ${value1}\n`,
-  deleted: ({ key, value }, indent) => `${indent}- ${key}: ${value}\n`,
-  unchanged: ({ key, value }, indent) => `${indent}  ${key}: ${value}\n`,
-  added: ({ key, value }, indent) => `${indent}+ ${key}: ${value}\n`,
+  added: ({ key, value }, nestingLevel) => `${indentCalc(nestingLevel)}+ ${key}: ${valueChecker(value, nestingLevel)}\n`,
+  changed: ({ key, value1, value2 }, nestingLevel) => `${indentCalc(nestingLevel)}+ ${key}: ${valueChecker(value2, nestingLevel)}\n${indentCalc(nestingLevel)}- ${key}: ${valueChecker(value1, nestingLevel)}\n`,
+  deleted: ({ key, value }, nestingLevel) => `${indentCalc(nestingLevel)}- ${key}: ${valueChecker(value, nestingLevel)}\n`,
+  unchanged: ({ key, value }, nestingLevel) => `${indentCalc(nestingLevel)}  ${key}: ${valueChecker(value, nestingLevel)}\n`,
+  nested: ({ key, children }, nestingLevel, func) => `${indentCalc(nestingLevel)}${key}: ${func(children, nestingLevel)}\n`,
+
 };
 
-const finder = (el, indent, func) => {
+
+const finder = (el, nestingLevel, func) => {
   const { type } = el;
+  if (_.isObject(el)) {
+    const newNestingLevel = nestingLevel + 1;
 
-  return renderTypes[type](el, indent, func);
+    return renderTypes[type](el, newNestingLevel, func);
+  }
+
+  return renderTypes[type](el, nestingLevel);
 };
 
-const render = (ast, indent = '  ') => {
-  const newIndent = indent + douleIndent;
-  const rendered = ast.map((el) => finder(el, newIndent, render));
+const render = (ast, nestingLevel) => {
+  const rendered = ast.map((el) => finder(el, nestingLevel, render));
 
-  return `${indent}{ \n${rendered.join('')}${indent}}`;
+  return `{ \n${rendered.join('')}${indentCalc(nestingLevel - 1)}}`;
 };
 
-export default render;
+const startingTestingLevel = 1;
+
+export default (ast) => render(ast, startingTestingLevel);
