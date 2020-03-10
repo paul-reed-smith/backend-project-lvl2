46014555113d675ed075e489a65b7129699c9f4e
paul-reed-smith <forall@mm.st>
2020-03-10 18:49:21 +0300

    refact test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,28 +1,34 @@
 import fs from 'fs';
+import path from 'path';
 import gendiff from '../src';
 
+const getPath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+
+// const outputFormats = ['pretty', 'plain', 'json'];
+
 test.each(['json', 'yaml', 'ini'])('Input format: %s, output is: pretty', (format) => {
-  const first = `${__dirname}/__fixtures__/first.${format}`;
-  const second = `${__dirname}/__fixtures__/second.${format}`;
-  const expected = fs.readFileSync(`${__dirname}/__fixtures__/expectedPretty.txt`, 'utf-8');
+  const first = getPath(`first.${format}`);
+  const second = getPath(`second.${format}`);
+  const expected = fs.readFileSync(getPath('expectedPretty.txt'), 'utf-8');
   const generated = gendiff(first, second);
 
   expect(expected).toEqual(generated);
 });
 
+
 test('plain format', () => {
-  const first = `${__dirname}/__fixtures__/first.json`;
-  const second = `${__dirname}/__fixtures__/second.json`;
-  const expected = fs.readFileSync(`${__dirname}/__fixtures__/expectedPlain.txt`, 'utf-8');
+  const first = getPath('first.json');
+  const second = getPath('second.json');
+  const expected = fs.readFileSync(getPath('expectedPlain.txt'), 'utf-8');
   const generated = gendiff(first, second, 'plain');
 
   expect(expected).toEqual(generated);
 });
 
 test('json format', () => {
-  const first = `${__dirname}/__fixtures__/first.ini`;
-  const second = `${__dirname}/__fixtures__/second.ini`;
-  const expected = fs.readFileSync(`${__dirname}/__fixtures__/expectedJSON.txt`, 'utf-8');
+  const first = getPath('first.json');
+  const second = getPath('second.json');
+  const expected = fs.readFileSync(getPath('expectedJSON.txt'), 'utf-8');
   const generated = gendiff(first, second, 'json');
 
   expect(expected).toEqual(generated);
