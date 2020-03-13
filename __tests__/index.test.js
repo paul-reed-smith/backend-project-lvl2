ae69f613761dadc0623e3fbdbc6e6473950bd9c4
paul-reed-smith <forall@mm.st>
2020-03-14 02:25:58 +0300

    test refact

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,20 +4,23 @@ import calculateTheDifferences from '../src';
 
 const getPath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
 
-test.each(['json', 'yaml', 'ini'])('Input format: %s, output is: pretty', (format) => {
-  const first = getPath(`first.${format}`);
-  const second = getPath(`second.${format}`);
-  const expected = fs.readFileSync(getPath('prettyExpected.txt'), 'utf-8');
-  const generated = calculateTheDifferences(first, second);
+const types = [
+  ['json', 'pretty'],
+  ['ini', 'pretty'],
+  ['yaml', 'pretty'],
+  ['json', 'plain'],
+  ['ini', 'plain'],
+  ['yaml', 'plain'],
+  ['json', 'json'],
+  ['ini', 'json'],
+  ['yaml', 'json'],
+];
 
-  expect(expected).toEqual(generated);
-});
-
-test.each(['plain', 'json'])('Input format: json, output is: %s', (format) => {
-  const first = getPath('first.json');
-  const second = getPath('second.json');
-  const expected = fs.readFileSync(getPath(`${format}Expected.txt`), 'utf-8');
-  const generated = calculateTheDifferences(first, second, format);
+test.each(types)('Input format: %s, output format: %s', (inputFormat, outputFormat) => {
+  const first = getPath(`first.${inputFormat}`);
+  const second = getPath(`second.${inputFormat}`);
+  const expected = fs.readFileSync(getPath(`${outputFormat}Expected.txt`), 'utf-8');
+  const generated = calculateTheDifferences(first, second, outputFormat);
 
   expect(expected).toEqual(generated);
 });
