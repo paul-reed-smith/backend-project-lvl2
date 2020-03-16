0e24128866cd0ab369d040668a4c4e6858304e13
paul-reed-smith <forall@mm.st>
2020-03-16 15:43:33 +0300

    name changes and small refact

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,16 +10,19 @@ const actions = {
   added: ({ key, value }, path) => `Property '${path}${key}' was added with value: ${format(value)}`,
   deleted: ({ key }, path) => `Property '${path}${key}' was deleted`,
   changed: ({ key, value1, value2 }, path) => `Property '${path}${key}' was changed from ${format(value1)} to ${format(value2)}`,
-  nested: ({ key, children }, path, f) => f(children, `${path}${key}.`),
+  nested: ({ key, children }, path, func) => func(children, `${path}${key}.`),
   unchanged: () => [],
 };
 
-const render = (ast, path = '') => _.flattenDeep(
-  ast.map((el) => {
+const render = (ast, path = '') => {
+  const mapped = ast.map((el) => {
     const { type } = el;
-    const finded = actions[type];
-    return finded(el, path, render);
-  }),
-).join('\n');
+    const stringifyNode = actions[type];
+
+    return stringifyNode(el, path, render);
+  });
+
+  return _.flattenDeep(mapped).join('\n');
+};
 
 export default render;
