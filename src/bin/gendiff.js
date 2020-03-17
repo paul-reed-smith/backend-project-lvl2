50cfac75609fed29874c06bea46cbed00faea354
paul-reed-smith <forall@mm.st>
2020-03-17 18:17:15 +0300

    name fix, program update

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .version(version)
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format')
+  .option('-f, --format [type]', 'output format')
   .action((firstConfig, secondConfig) => {
     console.log(calculateTheDifferences(firstConfig, secondConfig, program.format));
   })
