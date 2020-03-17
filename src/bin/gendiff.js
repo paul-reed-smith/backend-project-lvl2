4ed8913dd7564d0787a2a9f70151001669a9c9cf
paul-reed-smith <forall@mm.st>
2020-03-17 17:49:33 +0300

    program fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,11 +5,11 @@ import { version } from '../../package.json';
 
 program
   .version(version)
-  .description('Calculates differences between two files')
-  .arguments('<<firstConfig> <secondConfig>')
-  .option('-V, --version        output the version number')
-  .option('-f, --format [type]  Output format')
-  .option('-h, --help           output usage information')
+  .description('Compares two configuration files and shows a difference')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-V, --version', 'Output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .option('-h, --help', 'Output usage information')
   .action((firstConfig, secondConfig) => {
     console.log(calculateTheDifferences(firstConfig, secondConfig, program.format));
   })
